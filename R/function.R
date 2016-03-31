13835d192dde147e1cd7168c560c111c5fc843b4
Francois Michonneau <francois.michonneau@gmail.com>
2016-03-31 17:55:20 -0400

    save functions

--- R/function.R
+++ R/function.R
@@ -6,13 +6,15 @@ mean_lifeExp_by_continent <- function(gpmndr) {
         )
 }
 
-plot_mean_lifeExp <- function(gpmndr_continent) {
+plot_mean_lifeExp <- function(gpmndr_continent, plot_file = "figures/mean_lifeExp_by_continent.png") {
     ggplot(gpmndr_continent, aes(x = year, y = mean_lifeExp, group = continent, colour = continent)) +
         geom_line() +
         xlab("Year") + ylab("Mean life expectency")
+    ggsave(filename = plot_file)
 }
 
-plot_by_country <- function(gpmndr, countries = c("South Africa", "Morocco", "Algeria", "Nigeria")) {
+plot_by_country <- function(gpmndr, countries = c("South Africa", "Morocco", "Algeria", "Nigeria"),
+                            plot_file = "figures/plot_by_country.png") {
 
     not_found <- countries[! countries %in% unique(gpmndr[["country"]])]
     if(length(not_found) > 0) {
@@ -26,5 +28,5 @@ plot_by_country <- function(gpmndr, countries = c("South Africa", "Morocco", "Al
         geom_line() +
         facet_wrap(~ country) +
         xlab("Year") + ylab("Life Expectency")
-
+    ggsave(filename = plot_file)
 }
